290458b5065876bb2f1ad72d25439320ee36106d
steb-vs <steb.vs@outlook.com>
2020-08-08 17:59:08 +0200

    Setup procedural model

---
+++ Gameplay/Procedural/Data/Enums/TileType.gd
@@ -0,0 +1,8 @@
+extends Node
+
+class_name TileType
+
+const Floor: int = 0
+const Wall: int = 1
+const Exit: int = 2
+const Window: int = 3
---
+++ Gameplay/Procedural/Data/TileInfo.gd
@@ -0,0 +1,7 @@
+extends Node
+
+class_name TileInfo
+
+var tile_type: int
+var room_type: int
+var room_id: int
