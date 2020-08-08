0b2dafb0799ffd70d659cd065214e361e38bcd71
Ordinatueur\RubenLaden <ruben.tordjman74@gmail.com>
2020-08-08 11:28:27 -0400

    Fixed player animations

--- Gameplay/Player/AnimationHandler.gd
+++ Gameplay/Player/AnimationHandler.gd
@@ -5,7 +5,7 @@ class_name AnimationHandler
 # Animation speed
 const IDLE_SPEED: float = 0.3
 const WALK_SPEED: float = 1.0
-const SPRINT_SPEED: float = 2.0
+const SPRINT_SPEED: float = 1.4
 
 enum STANCES {IDLE=0, WALK, SPRINT}
 const ANIMATIONS := ["idle", "walk", "walk"] # One per stance
@@ -32,6 +32,8 @@ func progress_animation_stance(moving: bool, sprinting: bool):
         STANCES.SPRINT:
             if not moving:
                 stance = STANCES.IDLE
+            elif not sprinting:
+                stance = STANCES.WALK
 
     if stance == old_stance: return
 
@@ -55,7 +57,7 @@ func progress_animation_stance(moving: bool, sprinting: bool):
 func _play_walk_animation(animation_name: String, speed: float = 1):
     if animator.current_animation != animation_name:
         animator.stop()
-    var animation := animator.get_animation(animation_name)
-    animation.loop = true
-    animator.play()
+        var animation := animator.get_animation(animation_name)
+        animation.loop = true
+    animator.playback_speed = speed
     animator.play(animation_name, -1, speed)
