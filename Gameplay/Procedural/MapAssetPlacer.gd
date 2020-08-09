ac9e345adc55e1ebe1f6330cdc0a726d832280e2
Paul Maréchal <marechap.info@gmail.com>
2020-08-08 23:22:17 -0400

    wire player on the map

--- Gameplay/Procedural/MapAssetPlacer.gd
+++ Gameplay/Procedural/MapAssetPlacer.gd
@@ -6,6 +6,8 @@ const __UP := Vector3(0, 1, 0)
 const __tiles := {}
 const __walls := {}
 
+var __wallHitbox: StaticBody
+
 
 func _init() -> void:
 	var tileLibrary := preload("res://Assets/Tile/TileLibrary.tscn").instance()
@@ -19,25 +21,34 @@ func _init() -> void:
 	__walls[RoomType.Bedroom] = wallLibrary.get_node("BedroomWall")
 	__walls[RoomType.Kitchen] = wallLibrary.get_node("KitchenWall")
 	__walls[RoomType.Hallway] = wallLibrary.get_node("HallwayWall")
+	__wallHitbox = wallLibrary.get_node("WallHitbox")
 
 
-func placeAssets(origin: Node, map: Array) -> void:
+func placeAssets(root: Node, map: Array) -> Vector3:
+	var firstTilePosition = null
 	var rnd = RandomNumberGenerator.new()
 	rnd.randomize()
 	for y in range(map.size()):
 		var sub: Array = map[y]
 		for x in range(sub.size()):
+			var position = Vector3(x, 0, y)
 			var tileInfo: TileInfo = sub[x]
 			if tileInfo == null:
 				continue
 			var block: MeshInstance
 			if tileInfo.tile_type == TileType.Wall:
 				block = __walls[tileInfo.room_type]
+				var hitbox = __wallHitbox.duplicate()
+				hitbox.transform.origin = position
+				root.add_child(hitbox)
 			elif tileInfo.tile_type == TileType.Floor:
 				block = __tiles[tileInfo.room_type]
+				if firstTilePosition == null:
+					firstTilePosition = position
 			else:
 				continue
 			var generated: MeshInstance = block.duplicate()
-			generated.transform.origin = Vector3(x, 0, y)
+			generated.transform.origin = position
 			#generated.rotate(__UP, __HALF_PI * rnd.randi_range(0, 3))
-			origin.add_child(generated)
+			root.add_child(generated)
+	return firstTilePosition
