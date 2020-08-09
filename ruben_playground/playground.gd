c01f7cc0a93fb6ace56ceb47e1f6887e8e3367fe
Ordinatueur\RubenLaden <ruben.tordjman74@gmail.com>
2020-08-09 17:38:52 -0400

    Main scene's player integration
    Fixed pick/interact mechanics.
    Attached scripts onto props to make them pickable/interactable.
    Integrated player's mechanics into the main scene.

--- ruben_playground/playground.gd
+++ ruben_playground/playground.gd
@@ -4,10 +4,13 @@ onready var interactables: Spatial = $Interactables
 
 
 func _ready():
+	var logo = load("res://icon.png")
 	var prop_data = {
-		"logo": load("res://icon.png"),
-		"name": "Relic",
-		"length": 2
+		"logo": logo,
+		"name": "Picked",
+		"length": 2,
+		"content_logo": logo,
+		"content_name": "Interacted"
 	}
 	for interactable in interactables.get_children():
 		interactable.init(prop_data)
